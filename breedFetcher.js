157fddf565c136d0250ebd9b54e465f89f88d8f2
Shujie Ma <shujie1st@gmail.com>
2023-02-25 21:55:46 -0500

    Refactor code and use Mocha & Chai to test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,26 @@
 const request = require('request');
 
-// user to specify the breed name using command-line argument
-const breedName = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    let err = null;
+    let desc = null;
+    if (error) {
+      err = error;
+    } else {
+      if (response.statusCode >= 200 && response.statusCode <= 299) {
+        // convert body from string to object
+        const data = JSON.parse(body);
+        if (data.length === 0) {
+          err = "The breed name is not found";
+        } else {
+          desc = data[0].description;
+        }
+      } else {
+        err = "Unexpected response";
+      }
+    }
+    callback(err, desc);
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-
-  // if error, print error and return
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  // convert body from string to object
-  const data = JSON.parse(body);
-
-  // if breed name is not found, it will returun an empty array
-  if (data.length === 0) {
-    console.log("The breed name is not found.");
-  } else {
-    console.log(data[0].description);
-  }
-});
+module.exports = { fetchBreedDescription };
